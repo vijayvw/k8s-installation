bf07bd6103e7e9a3cb944e433d816119a417363c
vijay <vijayvw@gmail.com>
2026-06-13 16:44:38 +0530

    change

--- master.sh
+++ master.sh
@@ -12,6 +12,7 @@ EOF
 sudo sysctl --system
 lsmod | grep br_netfilter                        
 lsmod | grep overlay
+
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -21,6 +22,7 @@ echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
 $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get update
 sudo apt-get install -y containerd.io
 sudo systemctl restart containerd
@@ -28,19 +30,26 @@ containerd config default > config.toml
 sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' config.toml
 sudo cp config.toml /etc/containerd
 sudo systemctl restart containerd
+
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl        
 sudo systemctl enable --now kubelet
 sudo swapoff -a
+
 sudo kubeadm init
 sudo ctr -n k8s.io images ls
-mkdir -p $HOME/.kube
-sudo cp  /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+REAL_USER=$(logname)
+
+mkdir -p /home/$REAL_USER/.kube
+cp /etc/kubernetes/admin.conf /home/$REAL_USER/.kube/config
+chown -R $REAL_USER:$REAL_USER /home/$REAL_USER/.kube
+
 kubectl apply -f https://reweave.azurewebsites.net/k8s/v1.29/net.yaml
 kubectl get pods -n kube-system
